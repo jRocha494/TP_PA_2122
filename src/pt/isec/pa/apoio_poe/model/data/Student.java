a8455e2a8557b827f5cfb6ee6e553d8d299a2233
Pedro <o.pedrofilipe.o@isec.pt>
2022-04-24 19:56:49 +0100

    Renamed Map from DataLogic

--- src/pt/isec/pa/apoio_poe/model/data/Student.java
+++ src/pt/isec/pa/apoio_poe/model/data/Student.java
@@ -9,7 +9,7 @@ public class Student {
     private double classification;
     private boolean internshipAccess;   // boolean representing whether this Student is eligible to apply for internships
     private boolean hasProposed;    // True if this student has proposed a project/internship, False if not (Students can only propose once)
-    
+
     public Student(long studentNumber, String name, String email, String course, String branch, double classification, boolean internshipAccess) {
         this.studentNumber = studentNumber;
         this.name = name;
