d343e46a962c9497db0b2539f4092a300616bd43
jRocha494 <a21280321@isec.pt>
2022-04-30 16:51:54 +0100

    StageTwo,added Application

--- src/pt/isec/pa/apoio_poe/model/data/Application.java
+++ src/pt/isec/pa/apoio_poe/model/data/Application.java
@@ -3,4 +3,30 @@ package pt.isec.pa.apoio_poe.model.data;
 import java.util.List;
 
 public class Application {
+    List<String> choosenProposals;
+    long studentNumber;
+
+    public Application(List<String> choosenProposals, long studentNumber) {
+        this.choosenProposals = choosenProposals;
+        this.studentNumber = studentNumber;
+    }
+
+    public List<String> getChoosenProposals() {
+        return choosenProposals;
+    }
+
+    public long getStudentNumber() {
+        return studentNumber;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(studentNumber);
+        for (String p : choosenProposals)
+            sb.append("," + p);
+
+        return sb.toString();
+    }
 }
