307f1cab961b2fe8500b9f60fffd581533925574
jRocha494 <a21280321@isec.pt>
2022-06-04 17:10:11 +0100

    Main JFX

--- src/pt/isec/pa/apoio_poe/Main.java
+++ src/pt/isec/pa/apoio_poe/Main.java
@@ -2,12 +2,12 @@ package pt.isec.pa.apoio_poe;
 
 import pt.isec.pa.apoio_poe.model.Manager;
 import pt.isec.pa.apoio_poe.ui.text.TextUI;
-import pt.isec.pa.apoio_poe.utils.PAInput;
 
 public class Main {
     public static void main(String[] args) {
         Manager m = new Manager();
         TextUI ui = new TextUI(m);
         ui.start();
+        //Application.launch(MainJFX.class, args);
     }
 }
---
+++ src/pt/isec/pa/apoio_poe/ui/gui/MainJFX.java
@@ -0,0 +1,15 @@
+package pt.isec.pa.apoio_poe.ui.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainJFX extends Application {
+    @Override
+    public void init() throws Exception {
+        super.init();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+    }
+}
